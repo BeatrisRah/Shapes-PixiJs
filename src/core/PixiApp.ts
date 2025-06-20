d483e2c6978339074e52a0ad576e0c1b6ba43d33
Beatris <beatrisraj7@gmail.com>
2025-06-20 21:16:08 +0300

    style: Fix some code formatting

--- src/core/PixiApp.ts
+++ src/core/PixiApp.ts
@@ -7,7 +7,12 @@ export class PixiApp extends Application {
     }
 
     public async begin(){
-        await this.init({resizeTo:window, background:'#1099bb'})
+        await this.init({
+            resizeTo:window, 
+            background:'#1099bb'})
+        
         document.body.appendChild(this.canvas)
     }
+
+    
 }
