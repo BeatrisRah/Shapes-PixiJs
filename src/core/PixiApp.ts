50db385962bc18bd858e49e3e3be0b5fd9e4cf85
Beatris <beatrisraj7@gmail.com>
2025-06-21 00:33:55 +0300

    feat: Add game scene to application

--- src/core/PixiApp.ts
+++ src/core/PixiApp.ts
@@ -1,15 +1,23 @@
 import { Application } from "pixi.js";
+import { Game } from "../scenes/Game";
 
 
 export class PixiApp extends Application {
+    private game: Game
     constructor() {
         super()   
+        this.game = new Game()
     }
 
     public async begin(){
         await this.init({
             resizeTo:window, 
             background:'#1099bb'})
+
+        this.stage.addChild(this.game)
+        this.game.start()
+
+        //if assest load 
         
         document.body.appendChild(this.canvas)
     }
